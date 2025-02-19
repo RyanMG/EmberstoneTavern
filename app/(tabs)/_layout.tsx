490a11f81d706a7f3442abbfc6f51faa825d80e5
David Ryan Hall <david.ryan.hall@gmail.com>
2025-02-19 08:52:21 -0800

    feat: initial folders and views (no content yet, just shells)

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5'
+
+import { Link, Tabs } from 'expo-router';
+import { Pressable } from 'react-native';
+
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '@/components/useColorScheme';
+import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+
+// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+}) {
+  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        // Disable the static render of the header on web
+        // to prevent a hydration error in React Navigation v6.
+        headerShown: useClientOnlyValue(false, true),
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Tavern',
+          tabBarIcon: ({ color }) => <TabBarIcon name="beer" color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="campaigns"
+        options={{
+          title: 'Campaigns',
+          tabBarIcon: ({ color }) => <TabBarIcon name="sitemap" color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />
+        }}
+      />
+    </Tabs>
+  );
+}
