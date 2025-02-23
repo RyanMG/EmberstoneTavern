5bde13100f300c66aff7b837017c9daa1a3d511e
David Ryan Hall <david.ryan.hall@gmail.com>
2025-02-23 00:23:48 -0800

    feat: logging for remote API for quicker testing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,8 @@
 import PageContainer from '@components/common/PageContainers';
 import PageTitle from '@components/common/PageTitle';
+import { Text } from 'react-native';
+
+const API_ROOT = process.env.EXPO_PUBLIC_API_ROOT_URL;
 
 export default function Tavern() {
   return (
@@ -7,6 +10,7 @@ export default function Tavern() {
       <PageTitle
         text="The Emberstone Tavern"
       />
+      <Text>{API_ROOT}</Text>
     </PageContainer>
   );
 }
