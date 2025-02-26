9b6a2128ea7d6f7dca78cba4f9efc04256c03866
David Ryan Hall <david.ryan.hall@gmail.com>
2025-02-26 12:28:52 -0800

    ui: button updates
     - use react native paper button instead of react native button
     - implement an icon only button option

--- components/common/forms/Button.tsx
+++ components/common/forms/Button.tsx
@@ -1,23 +1,45 @@
-import { Button as RNButton } from 'react-native';
+import { Button as PaperButton, IconButton } from 'react-native-paper';
 import Colors from '@/lib/constants/Colors';
 
 export default function Button({
   title,
   onPress,
   disabled = false,
+  compact = false,
+  iconName,
+  iconOnly = false,
   theme = 'primary'
 }: {
-  title: string,
-  onPress: () => void,
+  title?: string;
+  onPress: () => void;
   disabled?: boolean;
+  compact?: boolean;
+  iconName?: string;
+  iconOnly?: boolean;
   theme?: 'primary' | 'secondary' | 'destroy';
 }) {
+  if (iconOnly && iconName) {
+    return (
+      <IconButton
+        icon={iconName}
+        iconColor={theme === 'primary' ? Colors.BUTTON.PRIMARY : theme === 'secondary' ? Colors.BUTTON.SECONDARY : Colors.BUTTON.DESTROY}
+        size={20}
+        onPress={onPress}
+      />
+    )
+  }
+
   return (
-    <RNButton
-      title={title}
-      color={theme === 'primary' ? Colors.BUTTON.PRIMARY : theme === 'secondary' ? Colors.BUTTON.SECONDARY : Colors.BUTTON.DESTROY}
+    <PaperButton
+      buttonColor={theme === 'primary' ? Colors.BUTTON.PRIMARY : theme === 'secondary' ? Colors.BUTTON.SECONDARY : Colors.BUTTON.DESTROY}
+      textColor={Colors.TEXT.BASE}
+      icon={iconName}
       onPress={onPress}
+      mode="contained"
+      compact={compact}
       disabled={disabled}
-    />
+    >
+      {title}
+    </PaperButton>
   )
 }
